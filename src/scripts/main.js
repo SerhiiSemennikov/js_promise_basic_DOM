fb12d2072020501a2380083abfe341082676fa4d
SerhiiSemennikov <semennik@hotmail.com>
2024-10-29 21:38:50 +0100

    co1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const body = document.querySelector('body');
+
+const promise = new Promise((resolve, reject) => {
+  resolve(myDisplayer);
+
+  reject(myDisplayer2);
+});
+
+promise.then(
+  function (value) {
+    myDisplayer(value);
+  },
+  function (error) {
+    myDisplayer2(error);
+  },
+);
+
+function myDisplayer() {
+  logo.onclick = () => {
+    const div = document.createElement('div');
+
+    div.classList.add('message');
+
+    div.textContent = 'Promise was resolved!';
+    body.appendChild(div);
+  };
+}
+
+setTimeout(myDisplayer2, 3000);
+
+function myDisplayer2() {
+  const div = document.createElement('div');
+
+  div.classList.add('message');
+  div.classList.add('error-message');
+
+  div.textContent = 'Promise was rejected!';
+  body.appendChild(div);
+}
